1f3a73cfe02e31e0363de3f3cf8a4e18845226c5
yelugamrakesh <31204282+yelugamrakesh@users.noreply.github.com>
2024-08-15 11:40:25 +0530

    First_Push
    First_Push

---
+++ QA_Testing_Workstation_1/src/test/java/QA_Xpath/Printing_outline.java
@@ -0,0 +1,13 @@
+package QA_Xpath;
+
+public class Printing_outline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	 System.out.println("Tender");
+		
+	}
+
+}
